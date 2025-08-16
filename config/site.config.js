48680a9ac5c4b2fb95079dc5ab417a50daf4cb49
NoirHive <saimunsajal12@gmail.com>
2025-08-16 14:49:30 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -43,7 +43,9 @@ module.exports = {
   
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/NoirHive', '/🥟 Some test files/Protected route'],
+ "protectedRoutes": [
+  "/🌞 Private folder/NoirHive",
+],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:files@noirhive.onmicrosoft.com',
